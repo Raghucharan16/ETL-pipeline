50c8b56cc63fd2f2042227d4f514ac0ab42e3e70
Narra_Venkata_Raghu_Charan <narravenkataraghucharan@gmail.com>
2025-01-10 18:23:47 +0530

    changed device to cpu

--- app_BFL.py
+++ app_BFL.py
@@ -5,7 +5,7 @@ from diffusers import FluxPipeline
 
 model_id = "black-forest-labs/FLUX.1-dev" #you can also use `black-forest-labs/FLUX.1-dev`
 
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16, device="cuda:1")
 pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
 
 prompt = "A Boy Dancing in Rain in Indian Streets, bright colours"
